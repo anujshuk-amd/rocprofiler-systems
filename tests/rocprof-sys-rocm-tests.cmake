cbd132d3037d3860bd52b7cf8912926ed3d78fd8
anujshuk-amd <anujshuk@amd.com>
2025-05-21 19:07:39 +0530

    Update tests/rocprof-sys-rocm-tests.cmake
    Co-authored-by: David Galiffi <David.Galiffi@amd.com>

--- tests/rocprof-sys-rocm-tests.cmake
+++ tests/rocprof-sys-rocm-tests.cmake
@@ -85,7 +85,7 @@ rocprofiler_systems_add_test(
 
 if(ROCPROFSYS_USE_ROCM)
     set(NAVI_REGEX "gfx(10|11|12)[A-Fa-f0-9][A-Fa-f0-9]")
-    rocprofiler_sys_get_gfx_info(${NAVI_REGEX} NAVI_DETECTED)
+    rocprofiler_systems_get_gfx_archs(NAVI_DETECTED GFX_MATCH ${NAVI_REGEX} ECHO)
     message(STATUS "Output is :  ${NAVI_DETECTED}")
     if(NAVI_DETECTED)
         set(ROCPROFSYS_ROCM_EVENTS_TEST "SQ_WAVES")
